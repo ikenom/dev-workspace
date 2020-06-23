71249872d92a0429bb61b45b2a053f0f9f23c073
Ikenna Omekam <iomekam@github.com>
2020-06-22 17:24:41 -0700

    rename

--- scripts/helm-deploy.sh
+++
@@ -1,7 +0,0 @@
-docker build \
-  --build-arg CONTAINER_REGISTRY=ikenom.azurecr.io \
-  --build-arg CONTAINER_REGISTRY_USERNAME=ikenom \
-  --build-arg CONTAINER_REGISTRY_PASSWORD=JVBGYF5K6j8LA0oxGj1L9Ndw78/J4iH2 \
-  -t deploy-helm \
-  .
-docker run deploy-helm

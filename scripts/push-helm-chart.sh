2b3d62e0a88741b36a95e99109056c3dc907b6a1
Ikenna Omekam <iomekam@github.com>
2020-06-23 12:13:03 -0700

    ci change

--- scripts/push-helm-chart.sh
+++ scripts/push-helm-chart.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Parse arguments.
-while getopts 'c:u:n:p:' arg; do
+while getopts 'c:u:n:p:du:dc:dp:' arg; do
   case "${arg}" in
     c) CONTAINER_REGISTRY=${OPTARG} ;;
     u) CONTAINER_REGISTRY_USERNAME=${OPTARG} ;;
     p) CONTAINER_REGISTRY_PASSWORD=${OPTARG} ;;
+    dc) DOCKER_CONTAINER_REGISTRY=${OPTARG} ;;
+    du) DOCKER_CONTAINER_REGISTRY_USERNAME=${OPTARG} ;;
+    dp) DOCKER_CONTAINER_REGISTRY_PASSWORD=${OPTARG} ;;
   esac
 done
 
 HELM_CHART_NAME=dev-workspace
 
-echo $CONTAINER_REGISTRY_PASSWORD | docker login ${CONTAINER_REGISTRY} \
-  --username $CONTAINER_REGISTRY_USERNAME \
+echo $DOCKER_CONTAINER_REGISTRY_PASSWORD | docker login ${DOCKER_CONTAINER_REGISTRY} \
+  --username $DOCKER_CONTAINER_REGISTRY_USERNAME \
   --password-stdin
 
 docker build -t push-helm-chart . --file Dockerfile.push-helm-chart
